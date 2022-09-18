fc599370e52ffa8db87d2e11d6b16e132599fcc5
xclusivfred <xclusivfred@gmail.com>
2022-09-18 23:52:37 +0100

    modified file 6

--- js/6-arrays.js
+++ js/6-arrays.js
@@ -1,10 +1,11 @@
 //var student1 = "alfred";
 //var student2 = "ceasar";
 //var student3 = "prince";
+
 var students = ["alfred","ceasar","prince"];
 
 var naughtyList = [];
-naughtyList.push = (students[0]);
+naughtyList.push(students[0]);
 
 var index = naughtyList.indexOf("alfred");
 console.log(naughtyList);
